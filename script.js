e046e58eafb4921d2f136be0133bea03b27b8458
Nurin Nasuha <nnm121@ic.ac.uk>
2025-11-06 23:53:50 +0800

    Add cat images and click to change

--- script.js
+++ script.js
@@ -2,8 +2,13 @@
 
 const startBtn = document.getElementById("startBtn")
 const message = document.getElementById("message");
+const imageContainer = document.getElementById("imageContainer")
+const baseURL = "https://cataas.com/cat" //random image
+const favImages = {};
 
-let started = CSSFontFeatureValuesRule
+let started = false
+let currentIndex = 0;
+let totalImages = 20;
 
 startBtn.addEventListener("click",function(){
     started = !started
@@ -11,15 +16,39 @@ startBtn.addEventListener("click",function(){
     if (started) { //Start swiping
         // Instruction message
         message.textContent = "Swipe right if you like a kitty and left if not really! :)"
+        
         startBtn.textContent = "Reset";
         startBtn.classList.add("reset");
+        currentIndex = 0;
+        showNextImage();
     }
     else{
         message.textContent = ""
         startBtn.textContent = "Start";
         startBtn.classList.remove("reset");
+
+        //remove image
+        imageContainer.innerHTML = "";
     }
 });
+
+function showNextImage(){
+    // check if reach limit
+    if (currentIndex >= totalImages){
+        message.textContent = "You've seen all the kitties! Here are your favs."
+        imageContainer.innerHTML = "";
+        return;
+    }
+
+    // display new cat and change when clicked
+    const img = document.createElement("img")
+    img.src = `${baseURL}?random=${Date.now()}`;
+    img.addEventListener("click", showNextImage);
+
+    imageContainer.innerHTML = "";
+    imageContainer.appendChild(img);
+    currentIndex++;
+}
 
 
 
